5ba2e1c1b6bc3efa39abebd6904048f7734f790f
john <100372654+leogadddd@users.noreply.github.com>
2025-06-22 08:13:22 +0000

    update github link

--- src/app/(app)/components/profilecard/socials.ts
+++ src/app/(app)/components/profilecard/socials.ts
@@ -22,7 +22,7 @@ const sociallinks: sociallinks[] = [
   },
   {
     name: "Github",
-    url: "https://github.com/LeoooooGadil",
+    url: "https://github.com/Leogaddd",
     Icon: TbBrandGithub,
   },
   {
